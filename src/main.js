2e2b3110d6e2c768c1b2bad583e86bda65ba1346
yangxiaoping <2878847069@qq.com>
2024-01-05 17:05:43 +0800

    feat: 引入element-ui

--- src/main.js
+++ src/main.js
@@ -6,8 +6,13 @@ import store from './store'
 import '@/assets/styles/reset.less'
 import '@/assets/styles/global.less'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import '@/components'
 import VueMarkdown from 'vue-markdown'
+
+Vue.use(ElementUI)
 Vue.component('Markdown', VueMarkdown)
 
 Vue.config.productionTip = false
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
 	<div class="home">
-		<p>home</p>
-		<router-link to="/demos">demos</router-link>
+		<el-link
+			type="primary"
+			@click="handleClick">
+			Demos
+		</el-link>
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'Home',
-	components: {}
+	methods: {
+		handleClick() {
+			this.$router.push({
+				name: 'Demos'
+			})
+		}
+	}
 }
 </script>
