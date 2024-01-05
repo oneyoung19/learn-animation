4f436541e8d99c67a654de74b62890c20c741193
yangxiaoping <2878847069@qq.com>
2024-01-05 12:18:49 +0800

    feat: 添加样式

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/styles/reset.less'
+import '@/assets/styles/global.less'
+
 import '@/components'
 import VueMarkdown from 'vue-markdown'
 Vue.component('Markdown', VueMarkdown)
