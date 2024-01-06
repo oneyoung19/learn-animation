eb5bb523f6e15748ecee037c41b910daaed1244b
yangxiaoping <2878847069@qq.com>
2024-01-06 23:41:55 +0800

    feat: 修改demos展示效果

--- src/views/demos/index.vue
+++ src/views/demos/index.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="demos">
-		<div class="menu">
+		<div
+			v-show="$route.name === 'Demos'"
+			class="menu">
 			<RouterButton to="Demo1">文字加粗不影响整体布局宽度</RouterButton>
 			<RouterButton to="Demo2">图片揭露动画</RouterButton>
 			<RouterButton to="Demo3">环形平移动画</RouterButton>
