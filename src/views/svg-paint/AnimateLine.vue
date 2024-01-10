f566706714fbe0d0015505bdc82205e3426d0550
yangxiaoping <2878847069@qq.com>
2024-01-10 16:51:54 +0800

    feat: 添加AnimateLine组件

---
+++ src/views/svg-paint/AnimateLine.vue
@@ -0,0 +1,92 @@
+<template>
+	<div>
+		<svg
+			class="demo1"
+			width="300px">
+			<path
+				fill="transparent"
+				stroke-width="5"
+				d="M10 80 Q77.5 10, 145 80 T280 80"
+				class="path"></path>
+		</svg>
+		<svg
+			width="300px"
+			height="175px"
+			class="demo2">
+			<path
+				fill="transparent"
+				stroke="#000000"
+				stroke-width="4"
+				d="M10 80 Q 77.5 10, 145 80 T 280 80"
+				class="path"></path>
+			<path
+				fill="transparent"
+				stroke="#FF2851"
+				stroke-width="4"
+				d="M10 80 Q 77.5 10, 145 80 T 280 80"
+				class="line2"></path>
+			<path
+				fill="transparent"
+				stroke="#000000"
+				stroke-width="4"
+				d="M10 80 Q 77.5 10, 145 80 T 280 80"
+				class="line1"></path>
+		</svg>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {}
+	},
+	methods: {}
+}
+</script>
+
+<style scoped lang="less">
+.demo1 {
+	@keyframes dash {
+		from {
+			stroke-dashoffset: 320;
+		}
+		to {
+			stroke-dashoffset: 0;
+		}
+	}
+	path {
+		stroke: #000000;
+		stroke-dasharray: 320;
+		stroke-dashoffset: 0;
+		animation: dash 2s ease-in-out infinite;
+	}
+}
+.demo2 {
+	.line1 {
+		stroke-dasharray: 340;
+		stroke-dashoffset: 40;
+		animation: dash1 1.5s linear alternate infinite;
+	}
+	.line2 {
+		stroke-dasharray: 320;
+		stroke-dashoffset: 320;
+		animation: dash2 1.5s linear alternate infinite;
+	}
+	@keyframes dash1 {
+		from {
+			stroke-dashoffset: 360;
+		}
+		to {
+			stroke-dashoffset: 40;
+		}
+	}
+	@keyframes dash2 {
+		from {
+			stroke-dashoffset: 280;
+		}
+		to {
+			stroke-dashoffset: -40;
+		}
+	}
+}
+</style>
--- src/views/svg-paint/index.vue
+++ src/views/svg-paint/index.vue
@@ -2,20 +2,24 @@
 	<el-tabs
 		class="tabs"
 		tab-position="left">
+		<el-tab-pane label="AnimateLine">
+			<AnimateLine></AnimateLine>
+		</el-tab-pane>
 		<el-tab-pane label="AnimatePath">
 			<AnimatePath></AnimatePath>
 		</el-tab-pane>
-		<el-tab-pane label="B">B</el-tab-pane>
 		<el-tab-pane label="C">C</el-tab-pane>
 		<el-tab-pane label="D">D</el-tab-pane>
 	</el-tabs>
 </template>
 
 <script>
+import AnimateLine from './AnimateLine'
 import AnimatePath from './AnimatePath'
 
 export default {
 	components: {
+		AnimateLine,
 		AnimatePath
 	},
 	data() {
