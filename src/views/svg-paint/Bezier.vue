240a066a7c6b4318a5287d373c1eba309f371e8a
yangxiaoping <2878847069@qq.com>
2024-01-10 15:30:48 +0800

    feat: 添加bazier

---
+++ src/views/svg-paint/Bezier.vue
@@ -0,0 +1,82 @@
+<template>
+	<div class="bezier">
+		<svg
+			id="svg"
+			width="100%"
+			height="100%"
+			version="1.1">
+			<path
+				fill="transparent"
+				stroke="#888888"
+				stroke-width="1"
+				class="path"></path>
+		</svg>
+		<div class="ball"></div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {}
+	},
+	mounted() {
+		this.run()
+	},
+	methods: {
+		run() {
+			let svg = document.querySelector('#svg'),
+				path = svg.querySelector('path'),
+				ball = document.querySelector('.ball'),
+				// TODO:这里取的是document的长宽 其实是不合理的 应该取svg自身的宽高 因为path设置的坐标是参照svg元素 而并非整个文档
+				wWidth = document.documentElement.offsetWidth,
+				wHeight = document.documentElement.offsetHeight
+			let pathStr =
+				'M50 ' +
+				Math.floor(wHeight - 200) +
+				' Q' +
+				Math.floor(wWidth / 2) +
+				' 0 ' +
+				Math.floor(wWidth) +
+				' ' +
+				Math.floor(wHeight / 2)
+			path.setAttribute('d', pathStr)
+			ball.style.offsetPath = 'path("' + pathStr + '")'
+			console.log({
+				wWidth,
+				wHeight,
+				pathStr
+			})
+		}
+	}
+}
+</script>
+
+<style scoped lang="less">
+.bezier {
+	width: 100%;
+	height: 100%;
+	svg {
+		display: block;
+		position: absolute;
+	}
+	path {
+		stroke: rgba(255, 0, 0, 0.1);
+		stroke-dasharray: 4;
+	}
+	.ball {
+		width: 20px;
+		height: 20px;
+		background-color: red;
+		border-radius: 50%;
+		offset-distance: 0%;
+
+		animation: move-ball 2s cubic-bezier(0.88, 0.05, 0.55, 0.5) infinite;
+	}
+	@keyframes move-ball {
+		to {
+			offset-distance: 100%;
+		}
+	}
+}
+</style>
--- src/views/svg-paint/index.vue
+++ src/views/svg-paint/index.vue
@@ -2,7 +2,9 @@
 	<el-tabs
 		class="tabs"
 		tab-position="left">
-		<el-tab-pane label="A">A</el-tab-pane>
+		<el-tab-pane label="Bezier">
+			<Bezier></Bezier>
+		</el-tab-pane>
 		<el-tab-pane label="B">B</el-tab-pane>
 		<el-tab-pane label="C">C</el-tab-pane>
 		<el-tab-pane label="D">D</el-tab-pane>
@@ -10,7 +12,12 @@
 </template>
 
 <script>
+import Bezier from './Bezier'
+
 export default {
+	components: {
+		Bezier
+	},
 	data() {
 		return {}
 	},
@@ -22,5 +29,11 @@ export default {
 .tabs {
 	height: 100vh;
 	min-height: 100vh;
+	:deep(.el-tabs__content) {
+		height: 100%;
+		.el-tab-pane {
+			height: 100%;
+		}
+	}
 }
 </style>
