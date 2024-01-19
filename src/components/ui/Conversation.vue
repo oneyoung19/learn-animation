a7dc4fe4d2ab2d827c91b49232e60525b7e8d708
yangxiaoping <2878847069@qq.com>
2024-01-19 20:51:46 +0800

    style: 添加样式

--- src/components/ui/Conversation.vue
+++ src/components/ui/Conversation.vue
@@ -1,5 +1,7 @@
 <template>
-	<div class="conversation">conversation</div>
+	<div class="conversation">
+		<div class="header">Speech Synthesis</div>
+	</div>
 </template>
 
 <script>
@@ -16,7 +18,10 @@ export default {
 	--coversation-theme: rgb(110, 72, 170);
 	// [font-style] [font-variant] [font-weight] [font-size]/[line-height] [font-family]
 	--conversation-font: 16px monospace;
-	--coversation-color-white: rgb(245, 248, 251);
+	--coversation-color-background: rgb(245, 248, 251);
+	--coversation-color-title: rgb(255, 255, 255);
+	--coversation-color-question: rgb(255, 255, 255);
+	--coversation-color-answer: rgb(74, 74, 74);
 }
 </style>
 
@@ -26,13 +31,23 @@ export default {
 	z-index: 999;
 	width: 100%;
 	height: 100%;
-	color: var(--coversation-theme);
+	// color: var(--coversation-theme);
 	font: var(--conversation-font);
-	background: var(--coversation-color-white);
+	background: var(--coversation-color-background);
 	border-radius: 10px;
 	box-shadow: rgba(0, 0, 0, 0.15) 0px 12px 24px 0px;
 	overflow: hidden;
 	inset: initial;
 	transition: transform 0.3s ease 0s;
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		height: 56px;
+		background: var(--coversation-theme);
+		color: rgb(255, 255, 255);
+		padding: 0px 10px;
+		font-weight: bold;
+	}
 }
 </style>
