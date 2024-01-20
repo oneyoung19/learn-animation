d9389968f3f5c1aafbb955832548d1e7f162304f
yangxiaoping <2878847069@qq.com>
2024-01-20 20:22:53 +0800

    feat: 添加ConversationLoading组件

---
+++ src/components/ui/ConversationLoading.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="loading">
+		<span
+			v-for="item in sum"
+			:key="item"
+			:style="`animation-delay: ${item * 0.2}s`">
+			.
+		</span>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		sum: {
+			type: Number,
+			default: 3
+		}
+	},
+	data() {
+		return {}
+	},
+	methods: {}
+}
+</script>
+
+<style scoped lang="less">
+@keyframes loading {
+	0% {
+		opacity: 0.2;
+	}
+	20% {
+		opacity: 1;
+	}
+	100% {
+		opacity: 0.2;
+	}
+}
+.loading {
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+	> span {
+		animation: loading 1.4s infinite both;
+	}
+}
+</style>
