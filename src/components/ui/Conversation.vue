e59801c1d886ffb6f1293cabf8fdb6d011f21c56
yangxiaoping <2878847069@qq.com>
2024-01-20 14:27:45 +0800

    feat: 添加对话区域

--- src/components/ui/Conversation.vue
+++ src/components/ui/Conversation.vue
@@ -1,13 +1,47 @@
 <template>
 	<div class="conversation">
 		<div class="header">Speech Synthesis</div>
+		<ul class="conversation-list">
+			<li
+				v-for="(item, index) in conversationList"
+				:key="index"
+				class="conversation-row">
+				<div
+					v-if="item.type === 'question'"
+					class="conversation-item question">
+					<SvgIcon
+						name="avatar"
+						class="icon"></SvgIcon>
+					<div class="bubble">{{ item.text }}</div>
+				</div>
+				<div
+					v-else
+					class="conversation-item answer">
+					<div class="bubble">{{ item.text }}</div>
+					<SvgIcon
+						name="robot"
+						class="icon"></SvgIcon>
+				</div>
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script>
 export default {
 	data() {
-		return {}
+		return {
+			conversationList: [
+				{
+					type: 'question',
+					text: 'Please type a number'
+				},
+				{
+					type: 'answer',
+					text: '123'
+				}
+			]
+		}
 	},
 	methods: {}
 }
@@ -22,6 +56,9 @@ export default {
 	--coversation-color-title: rgb(255, 255, 255);
 	--coversation-color-question: rgb(255, 255, 255);
 	--coversation-color-answer: rgb(74, 74, 74);
+	--coversation-color-white: rgb(255, 255, 255);
+	--conversation-header-height: 56px;
+	--conversation-footer-height: 56px;
 }
 </style>
 
@@ -31,6 +68,7 @@ export default {
 	z-index: 999;
 	width: 100%;
 	height: 100%;
+	margin: 0 auto;
 	// color: var(--coversation-theme);
 	font: var(--conversation-font);
 	background: var(--coversation-color-background);
@@ -43,11 +81,74 @@ export default {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		height: 56px;
+		height: var(--conversation-header-height);
 		background: var(--coversation-theme);
-		color: rgb(255, 255, 255);
+		color: var(--coversation-color-white);
 		padding: 0px 10px;
 		font-weight: bold;
+	}
+	.conversation-list {
+		width: 100%;
+		height: calc(
+			100% - var(--conversation-header-height) -
+				var(--conversation-footer-height)
+		);
+		padding-top: 6px;
+		overflow-y: scroll;
+		.conversation-row {
+			margin-bottom: 10px;
+			.conversation-item {
+				display: flex;
+				align-items: flex-end;
+				.icon {
+					font-size: 46px;
+					// animation: 0.3s ease 0s 1 normal forwards running Lmuha;
+					height: 40px;
+					min-width: 40px;
+					padding: 3px;
+					// transform: scale(0);
+					// transform-origin: right bottom;
+					box-shadow: rgba(0, 0, 0, 0.15) 0px 1px 2px 0px;
+				}
+				.bubble {
+					position: relative;
+					padding: 12px;
+					// animation: 0.3s ease 0s 1 normal forwards running Lmuha;
+					box-shadow: rgba(0, 0, 0, 0.15) 0px 1px 2px 0px;
+					// display: inline-block;
+					font-size: 14px;
+					max-width: 60%;
+					// margin: 0px 0px 10px;
+					overflow: hidden;
+					// transform: scale(0);
+					// transform-origin: left bottom;
+				}
+				&.question {
+					justify-content: flex-start;
+					.icon {
+						margin-right: 6px;
+						border-radius: 50% 50% 0px;
+					}
+					.bubble {
+						background: var(--coversation-theme);
+						border-radius: 18px 18px 18px 0px;
+						color: var(--coversation-color-question);
+					}
+				}
+				&.answer {
+					justify-content: flex-end;
+					.icon {
+						margin-left: 6px;
+						border-radius: 50% 50% 50% 0px;
+					}
+					.bubble {
+						background: var(--coversation-color-white);
+						border-radius: 18px 18px 0px;
+						color: var(--coversation-color-answer);
+					}
+				}
+			}
+		}
 	}
 }
 </style>
