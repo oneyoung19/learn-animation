8053489d7b21647dc4223b5cdbaad26f32abb8ff
yangxiaoping <2878847069@qq.com>
2024-01-30 14:02:01 +0800

    feat: 确认scrollTrigger配置

--- src/views/gsap/ScrollTriggerHorizonal.vue
+++ src/views/gsap/ScrollTriggerHorizonal.vue
@@ -21,21 +21,31 @@ export default {
 	},
 	mounted() {
 		gsap.set('.massiveImage', {
-			backgroundImage: `url(https://source.unsplash.com/random/${
-				innerWidth * 3
-			}x${innerHeight})`
+			// backgroundImage: `url(https://source.unsplash.com/random/${
+			// 	innerWidth * 3
+			// }x${innerHeight})`
+			backgroundImage: `url(https://plus.unsplash.com/premium_photo-1664382465062-beb842dac1ec?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=929&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTcwNjU4NjgyMA&ixlib=rb-4.0.3&q=80&w=3252)`
 		})
 
 		gsap.to('.massiveImage', {
+			// 向左移动100%
 			xPercent: -100,
+			// 向右移动一个屏幕宽度
 			x: () => innerWidth,
+			// 动效
 			ease: 'none',
 			scrollTrigger: {
 				trigger: '.massiveImage',
+				// 当元素顶部与viewport顶部重合时 开始动画
 				start: 'top top',
+				// 此距离会影响pin区域的高度
 				end: () => innerWidth * 3,
+				// 渐进条形式
 				scrub: true,
+				// 固定
 				pin: true,
+				// 快照
+				// snap: 1,
 				invalidateOnRefresh: true,
 				anticipatePin: 1
 			}
@@ -53,11 +63,9 @@ export default {
 	background-position: center;
 }
 .header-section {
+	height: 100vh;
 	text-align: center;
-	padding: 0px 15px;
-	margin: 40vh auto 50vh;
-	padding-bottom: 50vh;
-	margin-bottom: 0;
+	padding: 40vh 0 0;
 	font-size: 2em;
 }
 </style>
