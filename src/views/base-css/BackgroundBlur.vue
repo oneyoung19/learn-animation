a79e35a624923f2f8fd50aede4433d8f6abd51a7
yangxiaoping <2878847069@qq.com>
2024-03-05 11:38:53 +0800

    style: 添加bg-blur

--- src/views/base-css/BackgroundBlur.vue
+++ src/views/base-css/BackgroundBlur.vue
@@ -1,5 +1,7 @@
 <template>
-	<div>BackgroundBlur</div>
+	<div class="cover bg-blur">
+		<h1>我是需要突出显示的内容</h1>
+	</div>
 </template>
 
 <script>
@@ -11,4 +13,37 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.cover {
+	width: 600px;
+	height: 300px;
+	position: relative;
+	text-align: center;
+	line-height: 300px;
+	color: white;
+	background: transparent url(https://picsum.photos/seed/picsum/600/300) center
+		center no-repeat;
+	background-size: cover;
+}
+
+.bg-blur {
+	position: relative;
+	z-index: 1;
+	overflow: hidden;
+	* {
+		position: relative;
+		z-index: 3;
+	}
+	&:after {
+		content: '';
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		left: 0;
+		top: 0;
+		background: inherit;
+		filter: blur(10px);
+		z-index: 2;
+	}
+}
+</style>
